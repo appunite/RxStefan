686cbf26aefd3045427b471d606ee54d29527f69
SzymonMrozek <szymon.mrozek.sm@gmail.com>
2018-03-22 10:48:47 +0100

    Initial commit

---
+++ RxStefan/RxStefan.h
@@ -0,0 +1,19 @@
+//
+//  RxStefan.h
+//  RxStefan
+//
+//  Created by Szymon Mrozek on 22.03.2018.
+//  Copyright © 2018 AppUnite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxStefan.
+FOUNDATION_EXPORT double RxStefanVersionNumber;
+
+//! Project version string for RxStefan.
+FOUNDATION_EXPORT const unsigned char RxStefanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxStefan/PublicHeader.h>
+
+
