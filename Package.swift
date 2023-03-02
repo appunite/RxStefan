dde55e18e320a6edf5bd22fec6397fa0b9c10660
Abhijeet Chaudhary <abhijeet.chaudhary@appunite.com>
2023-03-02 12:31:21 +0100

    set platfrom version to 14

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "RxStefan",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(
             name: "RxStefan",
